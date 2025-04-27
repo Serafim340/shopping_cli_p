29eb4cea125ec57e2d3b2c4e28ebc9cf1317b25c
Serafim340 <127326173+Serafim340@users.noreply.github.com>
2025-04-27 15:47:35 +0000

    add new features

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-number_add = int(input("Введите количество желаемых покупок: "))
+def add_item(cart, item):
+    cart.append(item)
+    print(f"Товар добавлен: {item}")
+
+def remove_item(cart, item):
+    if item in cart:
+        cart.remove(item)
+        print(f"Товар удален: {item}")
+    else:
+        print(f"Товар '{item}' не найден в корзине.")
+
+def show_cart(cart):
+    print("\nИтоговый список покупок:")
+    for item in cart:
+        print(f"- {item}")
+
 cart = []
+
+number_add = int(input("Введите количество желаемых покупок: "))
 i = 0
 while i < number_add:
-    cart.append(str(input("Введите наименование желаемого товара: ")))
-    i += 1
-print(len(cart))
-cart.sort()
-print(cart)
+    item_name = input("Введите наименование желаемого товара: ")
+    add_item(cart, item_name)
+    i += 1 
+
 if "пример_1" in cart:
-    cart.remove("пример_1")
-cart[-1] = "пример_2"
-for item in cart:
-    print(item)
+    remove_item(cart, "пример_1")
+
+if cart:  
+    cart[-1] = "пример_2"
+
+cart.sort()
+print(len(cart))
+
+show_cart(cart)
